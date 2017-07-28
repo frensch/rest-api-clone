ccc2a98d28138f8e0dd5017e0bc6843f2852fb0f
Thiago Frensch <thiago.frensch@tvglobo.com.br>
2017-07-28 20:31:23 -0300

    created start time and end time of recording. Backup files writen as gzip

--- recorder.js
+++ recorder.js
@@ -1,13 +1,16 @@
+const zlib = require('zlib');
 var express = require('express');
 var request = require('request');
 var app = express();
 var fs = require('fs');
 
 var cloningMode = true;
-const serverPort = 8888;
+const serverPort = 5000;
 const hostname = "api.sde.globo.com";
 const host = "https://api.sde.globo.com";
-const interval = 10*1000;
+const interval = 20*1000;
+const startTime = Date.now() + (12*60*60*1000);
+const endTime = Date.now() + (49*60*60*1000);
 
 const timeoutMinutes = 0;
 
@@ -35,7 +38,7 @@ function addToCache(url, body, headers_in, headers_out) {
 function getFromCache(url) {
   
   var reqItem = cache.filter(o => o.url === url);
-  console.log('reqitem',reqItem);
+  //console.log('reqitem',reqItem);
 
   if(reqItem !== undefined && reqItem.length > 0) {
     
@@ -60,8 +63,8 @@ function getRequest(url, headers, res) {
 
   request.get (options, function(err, req, body) {
     if(err) {
-    	console.log('error:', err);
-      return res.send(err, req.statusCode);
+      console.log('error:', err);
+      return res.send(err, (req && req.statusCode ? req.statusCode : 500));
     }
     res.set(req.headers);
     res.send(body);
@@ -72,32 +75,43 @@ function getRequest(url, headers, res) {
 app.use(function (req, res) {
   console.log ('get recebido');
   console.log('urlOriginal: ' + req.originalUrl);
-  console.log('headers:', req.headers);
+  //console.log('headers:', req.headers);
   if(req.originalUrl === '' || req.originalUrl === '/')
-  	return res.send('', 404);
+    return res.send('', 404);
 
   var body = getFromCache(req.originalUrl);
 
+  console.log('requisitando');
+  getRequest(req.originalUrl, req.headers, res);
+/*
   if(body !== null) {
     console.log('usando cache');
     res.send(body);
   } else {
     console.log('requisitando');
-    getRequest(req.originalUrl, req.headers, res);
+    getRequest(req.originalUrl, res);
   }
-
+*/
 });
 
 function saveBackup() {
+  var now = Date.now();
+  if(now > endTime)
+    return;
   setTimeout(saveBackup, interval);
+  if(now < startTime)
+    return;
   var json = JSON.stringify(cache);
-  fs.writeFile('backup/backup_' + Date.now() + '.json', json, 'utf8', function (err) {
-    if(err)
-      console.log('falha ao gravar arquivo', err);
+  zlib.gzip(json, function(err, buf) {
+    var b64 = new Buffer(buf).toString('base64');
+    fs.writeFile('backup/backup_' + Date.now() + '.json.gzip', b64.toString(), 'base64', function (err) {
+      if(err)
+        console.log('falha ao gravar arquivo', err);
+    });
   });
 };
 
 app.listen(serverPort, function() {
-	console.log('iniciado');
+  console.log('iniciado');
   setTimeout(saveBackup, interval);
 });

15a109ff49da5bf665a21d29022558c286cead10
Thiago Frensch <thiago.frensch@tvglobo.com.br>
2017-08-01 15:08:44 -0300

    Executando o backup a partir de configurações e execução do player.js

--- player.js
+++ player.js
@@ -2,102 +2,127 @@ var express = require('express');
 var request = require('request');
 var app = express();
 var fs = require('fs');
+const zlib = require('zlib');
 
 var cloningMode = true;
-const serverPort = 8888;
-const hostname = "api.sde.globo.com";
-const host = "https://api.sde.globo.com";
-const interval = 10*1000;
-
-const timeoutMinutes = 0;
-
+var serverPort = 5001;
+var hostname = "api.sde.globo.com";
+var host = "https://api.sde.globo.com";
+var indexFile = 0;
+const startTime = 1501441122517;
+const endTime = 1501459669138;
+const backupFolder = './backup/';
+const timeOffset = Date.now() + 5000;
 app.use(express.bodyParser());
-
+var nextCache = [];
+const gmt = -3;
 
 var cache = [];
+var backupList = [];
 
-function addToCache(url, body, headers_in, headers_out) {
+console.log(startTime + 60*60*1000);
 
-  var d = new Date();
-  d.setMinutes(d.getMinutes() + timeoutMinutes);
-
-  var item = {
-    url: url,
-    dateTimeout: d,
-    body: body,
-    headers: { in: headers_in, out: headers_out}
-  };
-
-  cache.push(item);
-  console.log('adicionado ao cache: ', item.url);
+function initIndex() {
+  var file = getFileName(startTime);
+  indexFile = findFileIndex(file);
+  readFile(backupFolder + backupList[indexFile], function(cacheTemp) {
+    nextCache = cacheTemp;
+    swapAndLoadCache();
+  });
 }
 
+function findFileIndex(name) {
+  return backupList.indexOf(name);
+}
+
+function getFileName(time) {
+  return 'backup_' + time + '.json.gzip'
+}
+
+function readFile(name, callback) {
+  console.log('FILE:', name);
+  let d = '';
+  fs.createReadStream(name)
+  .pipe(zlib.createGunzip())
+  .on('data', function (data){
+    d += data.toString()
+  })
+  .on('end', function (){
+    var cacheTemp = JSON.parse(d);
+    callback(cacheTemp);
+  })
+  .on('error', function(err) {
+    console.log('ERROR:', err);
+  })
+}
+
+
+function getFileList(callback) {
+  const fs = require('fs');
+
+  fs.readdir(backupFolder, (err, files) => {
+    backupList = files;
+    callback();
+  });
+}
 function getFromCache(url) {
   
   var reqItem = cache.filter(o => o.url === url);
-  console.log('reqitem',reqItem);
+  //console.log('reqitem',reqItem);
 
   if(reqItem !== undefined && reqItem.length > 0) {
-    
-    if(reqItem[0].dateTimeout > Date.now()) {
-      return reqItem[0].body;
-    } else {
-      cache = cache.filter(o => o.url !== url);
-    }
+    return reqItem[0];
   }
   return null;
-}
-
-
-function getRequest(url, headers, res) {
-  var uri = (host + url);//'http://api.sde.globo.com/esportes/futebol/modalidades/futebol_de_campo/categorias/profissional/campeonatos/campeonato-brasileiro/edicoes/brasileirao-2015/jogos?pagina=1';
-  headers.host = hostname;
-  headers["accept-encoding"] = undefined;
-  var options = {
-    url: uri,
-    headers: headers
-  };
-
-  request.get (options, function(err, req, body) {
-    if(err) {
-    	console.log('error:', err);
-      return res.send(err, req.statusCode);
-    }
-    res.set(req.headers);
-    res.send(body);
-    addToCache(url, body, headers, req.headers);
-  });
 }
 
 app.use(function (req, res) {
   console.log ('get recebido');
   console.log('urlOriginal: ' + req.originalUrl);
-  console.log('headers:', req.headers);
+  //console.log('headers:', req.headers);
   if(req.originalUrl === '' || req.originalUrl === '/')
   	return res.send('', 404);
 
-  var body = getFromCache(req.originalUrl);
+  var cacheReq = getFromCache(req.originalUrl);
 
-  if(body !== null) {
+  if(cacheReq !== null && cacheReq.body !== null && cacheReq.headers !== null) {
     console.log('usando cache');
-    res.send(body);
+    res.set(cacheReq.headers.out);
+    res.send(cacheReq.body);
   } else {
-    console.log('requisitando');
-    getRequest(req.originalUrl, req.headers, res);
+    console.log('não encontrado');
+    res.send('', 404);
   }
 
 });
 
-function saveBackup() {
-  setTimeout(saveBackup, interval);
-  var json = JSON.stringify(cache);
-  fs.writeFile('backup/backup_' + Date.now() + '.json', json, 'utf8', function (err) {
-    if(err)
-      console.log('falha ao gravar arquivo', err);
+function getTimeFromFileName(filename) {
+  return parseInt(filename.slice(7, 7+13), 10);
+}
+
+function swapAndLoadCache() {
+  cache = nextCache;
+  indexFile = indexFile + 1;
+  nextCache = readFile(backupFolder + backupList[indexFile], function(cacheTemp) {
+    nextCache = cacheTemp;
+    var nextTime = getTimeFromFileName(backupList[indexFile]);
+    var date = new Date(nextTime+(gmt*60*60*1000)).toISOString().
+    replace(/T/, ' ').      
+    replace(/\..+/, '');
+    console.log('novo arquivo', indexFile, backupList[indexFile], date);
   });
+}
+
+function getBackup() {
+  setTimeout(getBackup, 1000);
+  var nextTime = getTimeFromFileName(backupList[indexFile+1]);
+  if((Date.now() - timeOffset) >= (nextTime - startTime)) {
+    swapAndLoadCache();
+  }
 };
 
 app.listen(serverPort, function() {
 	console.log('iniciado');
-  setTimeout(saveBackup, interval);
+  getFileList(initIndex);  
+  setTimeout(getBackup, 5000);
 });
